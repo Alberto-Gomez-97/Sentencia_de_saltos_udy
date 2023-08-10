7dd9d209a5c33826549771cb6c89ce7c21781b96
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-10 08:53:33 -0600

    Add files via upload

---
+++ Sentencia_de_salto_break_y_continue_udy/Sentencia_de_salto_break_y_continue_udy/Program.cs
@@ -0,0 +1,12 @@
+﻿// See Sentencia de salto
+for(int i = 1; i <= 10; i++)
+{
+    //detener el bucle para pasar a otra seccion
+    if (i == 5)
+    {
+        //break;     se termina el bucle hasta el valor evaluado en el if
+        continue;   //continua a pesar del valor a evaluar incluso omitiendo dicho valor mencionado
+    }
+    Console.WriteLine(i);
+}
+Console.WriteLine("fin");
